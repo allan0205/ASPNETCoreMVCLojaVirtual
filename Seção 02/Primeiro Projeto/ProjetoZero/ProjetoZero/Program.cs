6287be5dbdcc513aa6aecc8d7ce955ce9a38d83c
allan0205 <allan_sousa0205@hotmail.com>
2021-06-12 10:11:23 -0300

    Primeiro projeto ASP.NET Core MVC

---
+++ Seção 02/Primeiro Projeto/ProjetoZero/ProjetoZero/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
